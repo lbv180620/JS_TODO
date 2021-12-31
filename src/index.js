34685ee79699b50ea6a5639ad607129369125213
hashiudo <79960713+lbv180620@users.noreply.github.com>
2021-12-31 16:11:13 +0900

    save

--- src/index.js
+++ src/index.js
@@ -29,23 +29,48 @@ const onClickAdd = () => {
 
   // ⑤inputTextを元に、li>list-rowのDOM作成していく
 
-  // (1)liタグ生成
+  // ⑴liタグ生成
   const li = document.createElement("li");
   // console.log(li); // <li></li>
 
-  // (2)divタグ生成
+  // ⑵divタグ生成
   // JSでDOMを作成する場合はcreateElement("タグ名")を使う
   const div = document.createElement("div");
   // console.log(div); // <div></div>
 
-  // (3)divタグにlist-rowクラスを付与
+  // ⑶divタグにlist-rowクラスを付与
   div.className = "list-row";
   // console.log(div); // <div class="list-row"></div>
 
-  // (4)pタグを生成し、その中にinputTextを挿入する
+  // ⑷pタグを生成し、その中にinputTextを挿入する
   const p = document.createElement("p");
   p.innerText = inputText;
   // console.log(p); // <p>aaaaa</p>
+
+  // (11)完了ボタンと削除ボタンに対するイベントは一つずつidを振る訳ではないので、関数内で設定する
+  // ⑻buttonタグ(完了)生成
+  const completeBtn = document.createElement("button");
+  completeBtn.innerText = "完了";
+  completeBtn.addEventListener(
+    "click",
+    () => {
+      alert("完了");
+    },
+    false
+  );
+  // console.log(completeBtn); // <button>完了</button>
+
+  // ⑼buttonタグ(削除)生成
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "削除";
+  deleteBtn.addEventListener(
+    "click",
+    () => {
+      alert("削除");
+    },
+    false
+  );
+  // console.log(deleteBtn); // <button>削除</button>
 
   // ⑸liタグの子要素にdivタグを設定する
   li.appendChild(div);
@@ -55,18 +80,27 @@ const onClickAdd = () => {
   //</li>
 
   // ⑹divタグの子要素にpタグを設定
+  // appendChildはどんどん下に追加されていく
   div.appendChild(p);
-  console.log(li);
+  // console.log(li);
   //<li>
   // <div class="list-row">
   // <p>aaa</p>
   // </div>
   //</li>
 
+  // ⑽divタグの子要素にbuttonタグを設定
+  div.appendChild(completeBtn);
+  div.appendChild(deleteBtn);
+
   //⑺li>list-rowを未完了のulタグに挿入する
   // そのためにulタグに目印を付ける
   document.querySelector("#incompleted-list").appendChild(li);
 };
+
+// 機能2：完了ボタンを押す→
+
+// 機能3：削除ボタンを押す→未完了エリアにタスクが追加される
 
 // ①add-btn要素に対してclickイベントが起こるようにする
 // 追加ボタンと処理を紐づける目印がhtml側に必要
@@ -82,4 +116,5 @@ document
  * 入れ子にしたい時は、appendChild
  * などを使用して画面の要素を書き換えていく
  * という流れになる。
+ * DBに保存していないので、更新すると消える
  */
